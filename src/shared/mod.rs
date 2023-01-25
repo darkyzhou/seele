dbd2813a0533638c911717a795caa76681d966fa
darkyzhou <darkyoooooo@gmail.com>
2023-01-25 16:42:50 +0800

    feat(shared): improve `collect_output`

--- src/shared/mod.rs
+++ src/shared/mod.rs
@@ -1,3 +1,4 @@
+use either::Either;
 use std::{iter, process::Output};
 
 pub mod cond_group;
@@ -9,17 +10,44 @@ pub fn random_task_id() -> String {
     nano_id::base62::<8>()
 }
 
-#[inline]
+macro_rules! skip_if_empty {
+    ($source:expr, $iter:expr) => {
+        if $source.is_empty() {
+            Either::Left(iter::empty())
+        } else {
+            Either::Right($iter)
+        }
+    };
+}
+
+macro_rules! ellipse {
+    ($source:expr, $max_len:expr) => {
+        if $source.len() < $max_len {
+            Either::Left($source.iter())
+        } else {
+            Either::Right(
+                $source
+                    .iter()
+                    .take($max_len / 2)
+                    .chain(b"...".iter())
+                    .chain($source.iter().skip($max_len / 2)),
+            )
+        }
+    };
+}
+
 pub fn collect_output(output: &Output) -> String {
     const MAX_LEN: usize = 400;
 
-    let output = output
-        .stdout
-        .iter()
-        .chain(iter::once(&b'\n'))
-        .chain(output.stderr.iter())
-        .take(MAX_LEN)
-        .copied()
-        .collect::<Vec<_>>();
+    let output = skip_if_empty!(
+        output.stdout,
+        b"\n--- stdout ---\n".iter().chain(ellipse!(output.stdout, MAX_LEN))
+    )
+    .chain(skip_if_empty!(
+        output.stderr,
+        b"\n--- stderr ---\n".iter().chain(ellipse!(output.stderr, MAX_LEN))
+    ))
+    .copied()
+    .collect::<Vec<_>>();
     String::from_utf8_lossy(&output[..]).into_owned()
 }
