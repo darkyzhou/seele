b20c12800734d7a3af531f00a182f10c84847be2
darkyzhou <darkyoooooo@gmail.com>
2023-01-26 13:43:45 +0800

    chore(worker/run_judge/run): do not add `MOUNT_DIRECTORY` to paths if `executable` is not specified

--- src/worker/action/run_container/run_judge/run.rs
+++ src/worker/action/run_container/run_judge/run.rs
@@ -13,23 +13,25 @@ pub async fn run(
     let run_container_config = {
         let mut run_container_config = config.run_container_config.clone();
 
-        if let Some(paths) = run_container_config.paths.as_mut() {
-            paths.push(MOUNT_DIRECTORY.to_string());
-        } else {
-            run_container_config.paths = Some(vec![MOUNT_DIRECTORY.to_string()]);
-        }
+        if !config.executable.is_empty() {
+            if let Some(paths) = run_container_config.paths.as_mut() {
+                paths.push(MOUNT_DIRECTORY.to_string());
+            } else {
+                run_container_config.paths = Some(vec![MOUNT_DIRECTORY.to_string()]);
+            }
 
-        run_container_config.mounts.extend(
-            config
-                .executable
-                .iter()
-                .map(|file| runj::MountConfig {
-                    from: ctx.submission_root.join(file),
-                    to: [MOUNT_DIRECTORY, file].iter().collect(),
-                    options: Some(vec!["exec".to_string()]),
-                })
-                .map(MountConfig::Full),
-        );
+            run_container_config.mounts.extend(
+                config
+                    .executable
+                    .iter()
+                    .map(|file| runj::MountConfig {
+                        from: ctx.submission_root.join(file),
+                        to: [MOUNT_DIRECTORY, file].iter().collect(),
+                        options: Some(vec!["exec".to_string()]),
+                    })
+                    .map(MountConfig::Full),
+            );
+        }
 
         run_container_config
     };
