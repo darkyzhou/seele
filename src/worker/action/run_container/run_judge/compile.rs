8b24947f637699bcacfb447a036447c6584fde58
darkyzhou <darkyoooooo@gmail.com>
2023-01-26 12:47:44 +0800

    fix(worker/run_judge/compile): fix file permissions

--- src/worker/action/run_container/run_judge/compile.rs
+++ src/worker/action/run_container/run_judge/compile.rs
@@ -5,9 +5,9 @@ use crate::{
     worker::{run_container, runj, ActionContext, MountConfig},
 };
 use anyhow::{bail, Context};
-use std::path::PathBuf;
+use std::{fs::Permissions, os::unix::prelude::PermissionsExt, path::PathBuf};
 use tokio::fs;
-use tracing::instrument;
+use tracing::{instrument, warn};
 
 #[instrument]
 pub async fn compile(
@@ -15,54 +15,64 @@ pub async fn compile(
     config: &ActionCompileConfig,
 ) -> anyhow::Result<ActionExecutionReport> {
     let mount_directory = conf::PATHS.temp_mounts.join(nano_id::base62::<8>());
-    fs::create_dir_all(&mount_directory).await?;
+    fs::create_dir(&mount_directory).await?;
 
-    let run_container_config = {
-        let mut run_container_config = config.run_container_config.clone();
-        run_container_config.cwd = PathBuf::from(MOUNT_DIRECTORY);
+    let result = async {
+        // XXX: 0o777 is mandatory. The group bit is for rootless case and the others bit is for rootful case.
+        fs::set_permissions(&mount_directory, Permissions::from_mode(0o777)).await?;
 
-        run_container_config.mounts.extend(
-            config
-                .source
-                .iter()
-                .map(|file| runj::MountConfig {
-                    from: ctx.submission_root.join(file),
-                    to: [MOUNT_DIRECTORY, file].iter().collect(),
-                    options: None,
-                })
-                .map(MountConfig::Full),
-        );
+        let run_container_config = {
+            let mut run_container_config = config.run_container_config.clone();
+            run_container_config.cwd = PathBuf::from(MOUNT_DIRECTORY);
 
-        run_container_config.mounts.push(MountConfig::Full(runj::MountConfig {
-            from: mount_directory.clone(),
-            to: PathBuf::from(MOUNT_DIRECTORY),
-            options: None,
-        }));
+            run_container_config.mounts.push(MountConfig::Full(runj::MountConfig {
+                from: mount_directory.clone(),
+                to: PathBuf::from(MOUNT_DIRECTORY),
+                options: Some(vec!["rw".to_string()]),
+            }));
 
-        run_container_config
-    };
+            run_container_config.mounts.extend(
+                config
+                    .source
+                    .iter()
+                    .map(|file| runj::MountConfig {
+                        from: ctx.submission_root.join(file),
+                        to: [MOUNT_DIRECTORY, file].iter().collect(),
+                        options: None,
+                    })
+                    .map(MountConfig::Full),
+            );
 
-    let report = run_container(ctx, &run_container_config).await?;
+            run_container_config
+        };
 
-    for file in &config.save {
-        let source = mount_directory.join(file);
-        let target = ctx.submission_root.join(file);
-        let metadata = fs::metadata(&source)
-            .await
-            .with_context(|| format!("The file `{}` to save does not exist", file))?;
+        let report = run_container(ctx, &run_container_config).await?;
 
-        if metadata.is_file() {
-            fs::hard_link(source, target).await?;
-            continue;
-        } else if metadata.is_dir() {
-            bail!("Saving a directory is currently unsupported: {}", file);
-        } else if metadata.is_symlink() {
-            bail!("Saving a symlink is currently unsupported: {}", file);
+        for file in &config.save {
+            let source = mount_directory.join(file);
+            let target = ctx.submission_root.join(file);
+            let metadata = fs::metadata(&source)
+                .await
+                .with_context(|| format!("The file `{}` to save does not exist", file))?;
+
+            if metadata.is_file() {
+                fs::copy(source, target).await.context("Error copying the file")?;
+                continue;
+            } else if metadata.is_dir() {
+                bail!("Saving a directory is currently unsupported: {}", file);
+            } else if metadata.is_symlink() {
+                bail!("Saving a symlink is currently unsupported: {}", file);
+            }
+            bail!("Unknown file type: {}", file);
         }
-        bail!("Unknown file type: {}", file);
+
+        anyhow::Ok(report)
+    }
+    .await;
+
+    if let Err(err) = fs::remove_dir_all(&mount_directory).await {
+        warn!(directory = %mount_directory.display(), "Error removing mount directory: {:#}", err)
     }
 
-    fs::remove_dir_all(mount_directory).await?;
-
-    Ok(report)
+    result
 }
