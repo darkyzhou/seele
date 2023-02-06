3094a1b9c3317caa3f62b94c6e99ed0f896b1aa7
darkyzhou <darkyoooooo@gmail.com>
2023-02-06 11:36:33 +0800

    fix(runj/cgroupfs): create cgroup directory

--- runj/cmd/runj/cgroup/cgroupfs.go
+++ runj/cmd/runj/cgroup/cgroupfs.go
@@ -1,11 +1,22 @@
 package cgroup
 
 import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/darkyzhou/seele/runj/cmd/runj/utils"
 	"github.com/opencontainers/runc/libcontainer/cgroups/fs2"
 )
 
 // Initialize a new cgroup v2 directory via cgroupfs.
 // Mainly used for containerized environments with the help of sysbox.
 func GetCgroupPathViaFs() (string, error) {
-	return fs2.UnifiedMountpoint, nil
+	cgroupPath := path.Join(fs2.UnifiedMountpoint, "runj-containers", utils.RunjInstanceId)
+
+	if err := os.MkdirAll(cgroupPath, 0775); err != nil {
+		return "", fmt.Errorf("Failed to create cgroup directory: %w", err)
+	}
+
+	return cgroupPath, nil
 }
