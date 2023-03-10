1fedcbc15690a036fffa55fc1b61c5a347e81296
darkyzhou <darkyoooooo@gmail.com>
2023-03-10 10:16:39 +0800

    fix(runj/utils/overlayfs): add `xino=off` and `index=off` mount options

--- runj/cmd/runj/utils/overlayfs.go
+++ runj/cmd/runj/utils/overlayfs.go
@@ -20,7 +20,13 @@ func SetupOverlayfs() error {
 		return fmt.Errorf("Error deserializing the overlayfs config: %w", err)
 	}
 
-	options := fmt.Sprintf("userxattr,lowerdir=%s,upperdir=%s,workdir=%s", config.LowerDirectory, config.UpperDirectory, config.WorkDirectory)
+	// * `userxattr` is mandatory when using overlayfs with user namespace.
+	//    Note that when using tmpfs directories as upperdirs, overlayfs will complain 'failed to set xattr on upper'
+	//    because tmpfs does NOT support user extended attributes(user xattrs).
+	// * `xino=off` is used to prevent overlayfs from complaining that some filesystem 'does not support file handles'
+	//    which seems to because runj lacks CAP_DAC_READ_SEARCH.
+	// * `index=off` is used as Moby also sets it.
+	options := fmt.Sprintf("userxattr,xino=off,index=off,lowerdir=%s,upperdir=%s,workdir=%s", config.LowerDirectory, config.UpperDirectory, config.WorkDirectory)
 	if err := unix.Mount("overlay", config.MergedDirectory, "overlay", 0, options); err != nil {
 		return fmt.Errorf("Error creating overlayfs mount: %w", err)
 	}
