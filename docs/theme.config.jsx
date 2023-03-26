79a6fdcc1e22f97bd5a9477a11da82a45ec73697
darkyzhou <darkyoooooo@gmail.com>
2023-03-26 22:27:02 +0800

    docs: wip

---
+++ docs/theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+  logo: <span>Seele</span>,
+  project: {
+    link: "https://github.com/darkyzhou/seele",
+  },
+  i18n: [
+    { locale: "en", text: "English" },
+    { locale: "zh", text: "中文" },
+  ],
+};
