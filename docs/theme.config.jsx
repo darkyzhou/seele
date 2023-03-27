f449cb473dd4e6687752147794bb5ee88fe06e05
darkyzhou <darkyoooooo@gmail.com>
2023-03-27 12:33:37 +0800

    docs: wip

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,10 +1,49 @@
 export default {
-  logo: <span>Seele</span>,
+  docsRepositoryBase: "https://github.com/darkyzhou/seele/tree/main/docs",
+  logo: (
+    <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+      <img src="/logo.svg" width="30px" />
+      Seele
+    </div>
+  ),
   project: {
     link: "https://github.com/darkyzhou/seele",
   },
+  search: {
+    component: null,
+  },
   i18n: [
-    { locale: "en", text: "English" },
+    { locale: "en", text: "English (WIP)" },
     { locale: "zh", text: "中文" },
   ],
+  feedback: {
+    content: "文档有问题？欢迎反馈 →",
+  },
+  editLink: {
+    text: "编辑此页面",
+  },
+  footer: {
+    text: (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <span>别把看重的东西附上无边的价值</span>
+        <span>
+          Made with ❤️ by{" "}
+          <a
+            style={{ textDecoration: "underline" }}
+            href="https://darkyzhou.net"
+            target="_blank"
+            rel="noopener"
+          >
+            darkyzhou
+          </a>
+        </span>
+      </div>
+    ),
+  },
 };
