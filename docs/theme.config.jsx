8f5031db40b30b02b9b8d8d740bd98a1605721e5
darkyzhou <darkyoooooo@gmail.com>
2023-03-27 15:48:50 +0800

    docs: fix docs i18n

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,5 +1,32 @@
 export default {
   docsRepositoryBase: "https://github.com/darkyzhou/seele/tree/main/docs",
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - Seele Docs",
+    };
+  },
+  head: (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+    </>
+  ),
   logo: (
     <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
       <img src="/logo.svg" width="30px" />
